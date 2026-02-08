3dd04db957b0540c363ffa564299e95b18891587
aakarsh545 <aakarsh545@gmail.com>
2026-02-08 22:13:48 +0530

    fix: use same Rainbow Vibrant URL for Vibrant theme

---
+++ supabase/migrations/20260210080000_sync_vibrant_thumbnail.sql
@@ -0,0 +1,2 @@
+-- Set Vibrant theme to use same URL as Rainbow Vibrant
+UPDATE public.shop_items SET thumbnail_url = 'https://i.pinimg.com/736x/1d/d2/e3/1dd2e3d149c41232cc14331b1f67928e.jpg' WHERE name = 'Vibrant';
