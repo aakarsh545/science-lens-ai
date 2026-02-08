b4a4ac3384da832af3d26d39d76cecdd8a35687a
aakarsh545 <aakarsh545@gmail.com>
2026-02-08 22:17:13 +0530

    fix: update Rainbow Vibrant thumbnail to new URL

---
+++ supabase/migrations/20260210100000_update_rainbow_vibrant_thumbnail.sql
@@ -0,0 +1,4 @@
+-- Update ONLY Rainbow Vibrant thumbnail URL
+UPDATE public.shop_items
+SET thumbnail_url = 'https://i.pinimg.com/736x/21/c4/dc/21c4dcfcb5c7ddacfd41e3922d5370aa.jpg'
+WHERE name = 'Rainbow Vibrant';
